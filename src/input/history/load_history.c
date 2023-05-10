6e6d16042f1045092965f8f61b89b354e95c2475
César <cesar.zarcone@epitech.eu>
2023-05-10 22:13:08 +0200

    [~] Fix opening history file

--- src/input/history/load_history.c
+++ src/input/history/load_history.c
@@ -10,20 +10,20 @@
 void navigate_history(shell_t *shell, char *line)
 {
     if (shell->i->history_length == HISTORY_SIZE) {
-                free(shell->i->history[0]);
-                for (int j = 0; j < HISTORY_SIZE - 1; j++) {
-                    shell->i->history[j] = shell->i->history[j + 1];
-                } shell->i->history_length--;
+        free(shell->i->history[0]);
+        for (int j = 0; j < HISTORY_SIZE - 1; j++) {
+            shell->i->history[j] = shell->i->history[j + 1];
+        }
+        shell->i->history_length--;
     }
     shell->i->history[shell->i->history_length++] = strdup(line);
 }
 
 void line_comp(shell_t *shell, char *line, int *i)
 {
-    for (*i = 0; *i < shell->i->history_length; *i++) {
-        if (strcmp(line, shell->i->history[*i]) == 0) {
+    for (*i = 0; *i < shell->i->history_length; (*i)++) {
+        if (strcmp(line, shell->i->history[*i]) == 0)
             break;
-        }
     }
 }
 
@@ -33,14 +33,19 @@ void load_history(shell_t *shell)
     if (file == NULL)
         return;
     char *line = malloc(sizeof(char) * 1024);
-    if (fgets(line, sizeof(line), file) == NULL)
+    if (fgets(line, 1024, file) == NULL)
         shell->i->history[0] = NULL;
-    while (fgets(line, sizeof(line), file) != NULL) {
+    while (fgets(line, 1024, file) != NULL) {
         line[strcspn(line, "\r\n")] = '\0';
         int i;
         line_comp(shell, line, &i);
-        if (i == shell->i->history_length)
+        if (i == shell->i->history_length) {
             navigate_history(shell, line);
+        } else {
+            free(line);
+        }
+        line = malloc(sizeof(char) * 1024);
     }
     fclose(file);
+    free(line);
 }
