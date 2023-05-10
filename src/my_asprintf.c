3ddd6026035a4648ce1439b5c7223b21e2c0a135
Leo <leo.dumont@epitech.eu>
2023-05-10 18:50:52 +0200

    [*] Fix coding style, removed comments

--- src/my_asprintf.c
+++ src/my_asprintf.c
@@ -8,22 +8,20 @@
 #include "shell.h"
 #include <stdarg.h>
 
-int my_asprintf(char **strp, const char *format, ...) {
+int my_asprintf(char **strp, const char *format, ...)
+{
     va_list args;
     int size;
-
     va_start(args, format);
-    size = vsnprintf(NULL, 0, format, args) + 1; // +1 for null terminator
+
+    size = vsnprintf(NULL, 0, format, args) + 1;
     va_end(args);
-
     *strp = malloc(size);
-    if (!*strp) {
+    if (!*strp)
         return -1;
-    }
-
     va_start(args, format);
     vsnprintf(*strp, size, format, args);
     va_end(args);
 
-    return size - 1; // return length of string
+    return size - 1;
 }
