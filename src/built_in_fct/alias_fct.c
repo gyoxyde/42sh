e3513f8bddf406b4df6ee226d1e626b51c3bbaf2
Leprêtre <leo.lepretre@epitech.eu>
2023-05-11 14:41:56 +0200

    [~] fix alias function with a tab

--- src/built_in_fct/alias_fct.c
+++ src/built_in_fct/alias_fct.c
@@ -20,7 +20,6 @@ int check_which_alias(int number_av, char *str)
 
 void alias_no_av_fct(shell_t *shell)
 {
-    file_info(".alias", shell);
     if (shell->a->file != NULL) {
         for (int i = 0; shell->a->file[i] != NULL; i++) {
             my_printf("%s\n", shell->a->file[i]);
@@ -30,9 +29,11 @@ void alias_no_av_fct(shell_t *shell)
 
 void alias_existing(shell_t *shell, int size_line, int save, int pos)
 {
-    int size = strlen(shell->array[1]);
-    for (; my_strncmp(shell->array[1], shell->a->file[pos], size) != 0; pos++);
-    save = pos;
+    for (; shell->a->file[pos] != NULL; pos++) {
+        char **com = my_str_to_word_array(shell->a->file[pos], ' ');
+        if (my_strcmp(com[0], shell->array[1]) == 0)
+            save = pos;
+    }
     char *str = malloc (sizeof(char) * (size_line + shell->a->count + 1));
     for (int i = 1; shell->array[i] != NULL; i++) {
         if (i > 1)
@@ -40,25 +41,30 @@ void alias_existing(shell_t *shell, int size_line, int save, int pos)
         str = my_strcat(str, shell->array[i]);
     }
     shell->a->file[save] = str;
-    int fd2 = open(".alias",  O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    for (int i = 0; shell->a->file[i] != NULL; i++) {
-        write(fd2, shell->a->file[i], strlen(shell->a->file[i]));
-        write(fd2, "\n", 1);
-    }
-    close(fd2);
 }
 
-void alias_not_existing(shell_t *shell)
+void alias_not_existing(shell_t *shell, int size_line)
 {
-    int fd = open(".alias",  O_WRONLY | O_CREAT | O_APPEND, 0644);
+    int length = 0;
+    int y = 0;
+    char **alias_tab = NULL;
+    char *str = malloc(sizeof(char) * (size_line + shell->a->count + 1));
+    memset(str, 0, (size_line + shell->a->count));
     for (int i = 1; shell->array[i] != NULL; i++) {
-        write(fd, shell->array[i], strlen(shell->array[i]));
-        if (i < shell->a->count)
-            write(fd, " ", 1);
-        else
-            write(fd, "\n", 1);
+        if (i > 1)
+            str = my_strcat(str, " ");
+        str = my_strcat(str, shell->array[i]);
     }
-    close(fd);
+    if (shell->a->file != NULL) {
+        for (; shell->a->file[length] != NULL; length++);
+        alias_tab = malloc (sizeof(char *) * length + 2);
+        for (; shell->a->file[y] != NULL; y++)
+            alias_tab[y] = my_strdup(shell->a->file[y]);
+        alias_tab[y] = str; alias_tab[y + 1] = NULL;
+    } else {
+        alias_tab = create_tab_alias(str, alias_tab);
+    }
+    shell->a->file = alias_tab;
 }
 
 void alias_fct(shell_t *shell)
@@ -67,7 +73,6 @@ void alias_fct(shell_t *shell)
     int size_line = 0;
     int save = 0;
     int pos = 0;
-    file_info(".alias", shell);
     for (int i = 1; shell->array[i] != NULL; i++)
         size_line += strlen(shell->array[i]);
     for (int i = 1; shell->array[i] != NULL; i++)
@@ -75,6 +80,6 @@ void alias_fct(shell_t *shell)
     if (check_alias_exist(shell) == 24) {
         alias_existing(shell, size_line, save, pos);
     } else {
-        alias_not_existing(shell);
+        alias_not_existing(shell, size_line);
     }
 }
